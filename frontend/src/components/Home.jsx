868618bec77e770b15267634df6946d0fe33a6ad
Kunal Nailwal <nailwalkunal007@gmail.com>
2024-12-25 03:09:50 +0530

    all project setup with code

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
 // src/pages/Home.jsx
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
