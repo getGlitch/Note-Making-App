dc6ca73fdccb2c1bf463c7fdabf368859ac0907c
Kunal Nailwal <102874692+getGlitch@users.noreply.github.com>
2025-05-20 19:49:20 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,19 @@
+variable "region" {
+  default = "eu-north-1"
+}
 
+variable "project_name" {
+  default = "note-making-proj"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "ubuntu_ami" {
+  default = "ami-0c1ac8a41498c1a9c" # Ubuntu 20.04 for eu-north-1 (check before deploy)
+}
+
+variable "availability_zones" {
+  default = ["eu-north-1a", "eu-north-1b"]
+}
