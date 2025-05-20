4ae1348aa7769a43f597b15e1e525d0cb5f33c38
Kunal Nailwal <102874692+getGlitch@users.noreply.github.com>
2025-05-20 19:48:58 +0530

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+apt update -y
+apt install -y docker.io docker-compose git
+usermod -aG docker ubuntu
 
+# Clone the repo (replace with your public GitHub URL)
+cd /home/ubuntu
+sudo -u ubuntu git clone https://github.com/getGlitch/Note-Making-App app
+cd app
+
+# Run your Docker Compose stack
+sudo -u ubuntu docker-compose up -d
